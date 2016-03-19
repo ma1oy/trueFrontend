1b01d58d8b0fa247d874861b306d2ca56e4d5480
roman <sheva2012h@gmail.com>
2016-03-19 05:12:55 +0200

    Small modifications

--- gulpfile.js
+++ gulpfile.js
@@ -16,62 +16,53 @@ var gulp        = require('gulp'),
     cssmin      = require('gulp-clean-css'),    // CSS minimization
     imagemin    = require('gulp-imagemin'),     // Image minimization
     pngquant    = require('imagemin-pngquant'), // Lossy compression of PNG images
-    browserSync = require("browser-sync"),      // Browser synchronization
-    reload      = browserSync.reload,           // Browser page reload
-    YAML        = require('yamljs');
+    browserSync = require('browser-sync').create(),      // Browser synchronization
+    YAML        = require('yamljs'),            // YAML parser
+    reload      = browserSync.reload;           // Browser page reload
 
-// var mainFiles = {
-//     index: 'index.twig',
-//     script: 'main.js',
-//     style:  'style.scss'
-// };
-
-var mypath = {
+var dir = {
     config:     'config.yml',
+    mainFiles: {
+        markup: 'index.twig',
+        style:  'style.scss',
+        script: 'main.js'
+    },
     dest: {
-        home:   './build',
-        markup: 'build/',
-        css:    'build/',
-        js:     'build/',
-        svg:    'build/svg/',
-        img:    'build/img/'
+        home:   './build/',
+        markup: './build/',
+        style:  './build/',
+        script: './build/',
+        img:    './build/img/',
+        imgIn:  './build/svg/'
     },
     src: {
         home:   './src',
-        markup: 'src/templates/index.twig',
-        js:     'src/js/main.js',
-        css:    'src/css/style.scss',
-        svg:    'src/svg/**/[^_]*.svg',
-        img:    'src/img/**/[^_]*.*'
-    },
-    watch: {
-        markup:   'src/templates/*.twig', // ? [^_]
-        js:     'src/js/**/[^_]*.js',
-        css:    'src/css/**/*.scss', // ? [^_]
-        svg:    'src/svg/**/[^_]*.svg',
-        img:    'src/img/**/[^_]*.*'
+        markup: './src/templates/**/*.twig', // [^_]
+        style:  './src/styles/**/*.scss', // [^_]
+        script: './src/scripts/**/[^_]*.js',
+        img:    ['./src/images/**/[^_]*.*', '!./src/images/inline/**'],
+        imgIn:  './src/images/inline/**/[^_]*.svg'
     }
 };
 
-// var config = YAML.load(mypath.config);
+var config = YAML.load(dir.config);
 
-var config = YAML.load(mypath.config);
-
-// DEV SERVER CONFIG
-var serverConfig = {
-    server: {
-        baseDir: "./build"
-    },
-    //tunnel: true,
-    host: 'localhost',
-    port: 9000,
-    logPrefix: "EASY_FRONTEND"
-};
+var mainSrc = dir.src;
+for (var key in mainSrc) {
+    if (mainSrc.hasOwnProperty(key)) {
+        switch(key) {
+            case 'markup': case 'style': case 'script':
+            mainSrc[key] = mainSrc[key].split('/').slice(0, -2).join('/') + '/' + dir.mainFiles[key];
+                break;
+            default: break;
+        }
+    }
+}
 
 // SVG BUILD TASK
 gulp.task('svg:build', function () {
     return gulp
-        .src(mypath.src.svg)
+        .src(mainSrc.imgIn)
         .pipe(svgmin())                         // svg minimization
         .pipe(rename(function (path) {          // Rename files for beauty id's
             var name = path.dirname.split(path.sep);
@@ -80,53 +71,53 @@ gulp.task('svg:build', function () {
             path.basename = name.join('-');
         }))
         .pipe(svgstore({ inlineSvg: true }))    // svg concatenation into symbols
-        .pipe(gulp.dest(mypath.dest.svg));      // Save to build path
+        .pipe(gulp.dest(dir.dest.imgIn));       // Save to build path
 });
 
 // HTML BUILD WITH INLINE SVG TASK
-gulp.task('htmlsvg:build', ['svg:build'], function () {
+gulp.task('html-svg:build', ['svg:build'], function () {
     return gulp
-        .src(mypath.src.markup)
+        .src(mainSrc.markup)
         .pipe(twig({
             // data: YAML.parse(require('fs').readFileSync(config, 'utf8'))
-            data: YAML.load(mypath.config)
-            // data: config
+            // data: YAML.load(dir.config)
+            data: config
         }))
-        .pipe(gulp.dest(mypath.dest.markup));    // Save to build path
+        .pipe(gulp.dest(dir.dest.markup));      // Save to build path
 });
 
 // HTML BUILD
-gulp.task('html:build', ['htmlsvg:build'], function() {
-    return del([mypath.dest.svg]);             // Remove svg build folder
+gulp.task('html:build', ['html-svg:build'], function() {
+    return del([dir.dest.imgIn]);               // Remove svg build folder
 });
 
 // IMAGE BUILD TASK
-gulp.task('image:build', function () {
+gulp.task('img:build', function () {
     return gulp
-        .src(mypath.src.img)
+        .src(mainSrc.img)
         .pipe(imagemin({                        // Images minimization
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()],
             interlaced: true
         }))
-        .pipe(gulp.dest(mypath.dest.img));      // Save to build path
-        //.pipe(reload({stream: true}));          // Server reload
+        .pipe(gulp.dest(dir.dest.img));         // Save to build path
 });
 
 // CSS BUILD TASK
 gulp.task('css:build', function () {
     return gulp
-        .src(mypath.src.css)
+        .src(mainSrc.style)
         .pipe(sourcemaps.init())                // Maps initialization
-        .pipe(scss())                           // ssss compile
-        .pipe(prefixer({                        // Add vendor prefixes
-            browsers: ['last 5 versions']
-        }))
+        .pipe(scss())                           // scss compile
+        .pipe(prefixer(config.autoprefixer))
         .pipe(cssmin())                         // css minimization
         .pipe(sourcemaps.write())               // Write maps
-        .pipe(gulp.dest(mypath.dest.css));     // Save to build path
-        //.pipe(reload({stream: true}));          // Server reload
+        .pipe(rename({
+            basename:   'style',
+            suffix:     '.min'
+        }))
+        .pipe(gulp.dest(dir.dest.style));       // Save to build path
 });
 
 // CSS CLEAN TASK
@@ -137,24 +128,27 @@ gulp.task('css:clean', function() {
 // JS BUILD TASK
 gulp.task('js:build', function () {
     return gulp
-        .src(mypath.src.js)
+        .src(mainSrc.script)
         .pipe(rigger())                         // Add dependent files
         .pipe(sourcemaps.init())                // Maps initialization
         .pipe(uglify())                         // js minimization
         .pipe(sourcemaps.write())               // Write maps
-        .pipe(gulp.dest(mypath.dest.js));      // Save to build path
-        //.pipe(reload({stream: true}));          // Server reload
+        .pipe(rename({
+            basename:   'script',
+            suffix:     '.min'
+        }))
+        .pipe(gulp.dest(dir.dest.script));      // Save to build path
 });
 
 // CLEAN TASK
 gulp.task('clean', function() {
-    return del(mypath.dest.home);
+    return del(dir.dest.home);
     //return del(Object.keys(mypath.build).map(function (key) {return mypath.build[key]}));
 });
 
 // CONFIG UPDATE TASK
 gulp.task('config:update', function() {
-    config = YAML.load(mypath.config);
+    config = YAML.load(dir.config);
 });
 
 // REBUILD TASK
@@ -162,7 +156,7 @@ gulp.task('rebuild', [
     'html:build',
     'css:build',
     'js:build',
-    'image:build'
+    'img:build'
 ]);
 
 // BUILD TASK
@@ -170,18 +164,32 @@ gulp.task('build', ['clean'], function() {
     gulp.start('rebuild');
 });
 
+// // WEBSERVER TASK
+// gulp.task('live', function() {
+//     browserSync(config.browserSync);
+// });
+
 // WEBSERVER TASK
-gulp.task('live', function() {
-    browserSync(serverConfig);
+gulp.task('live', function () {
+    browserSync.init(config.browserSync);
 });
 
 // WATCH TASK
 gulp.task('watch', function(){
-    gulp.watch(mypath.config, ['config:update', 'build', reload]);
-    gulp.watch([mypath.watch.markup, mypath.watch.svg], ['html:build', reload]);
-    gulp.watch(mypath.watch.css, ['css:build', reload]);
-    gulp.watch(mypath.watch.js, ['js:build', reload]);
-    gulp.watch(mypath.watch.img, ['image:build', reload]);
+    gulp.watch(dir.config, ['config:update', 'build', reload]);
+    gulp.watch([dir.src.markup, dir.src.imgIn], ['html:build', reload]);
+    gulp.watch(dir.src.style, ['css:build', reload]);
+    gulp.watch(dir.src.script, ['js:build', reload]);
+    gulp.watch(dir.src.img, ['img:build', reload]);
+});
+
+gulp.task('clearCache', function() {
+    // Still pass the files to clear cache for
+    gulp.src('./*.*')
+        .pipe(cache.clear());
+
+    // Or, just call this for everything
+    cache.clearAll();
 });
 
 // DEFAULT TASK
