549c9ac7a1c74bcfbd26995a11cd75e32d63699a
roman <sheva2012h@gmail.com>
2016-03-25 22:38:51 +0200

    Improve plugins require

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,36 @@
 'use strict';
-
-var gulp        = require('gulp'),
-    YAML        = require('yamljs'),            // YAML parser
-    del         = require('del'),               // Files remove
-    gulpif      = require('gulp-if'),
-    twig        = require('gulp-twig'),         // twig parser
-    htmlmin     = require('gulp-htmlmin'),
-    //es          = require('event-stream'),      //"event-stream":       "^3.3.2",
-    //concat      = require('gulp-concat'),       // File concatenation // "gulp-concat":        "^2.6.0",
-    svgstore    = require('gulp-svgstore'),     // svg concatenation into symbol tags
-    svgmin      = require('gulp-svgmin'),       // svg minimization
-    rename      = require('gulp-rename'),       // File rename (for svg's id's)
-    rigger      = require('gulp-rigger'),       // Files including (//= path/name)
-    prefixer    = require('gulp-autoprefixer'), // Automatic adding prefix for CSS rules
-    uglify      = require('gulp-uglify'),       // js minimization
-    scss        = require('gulp-scss'),         // SASS parser
-    sourcemaps  = require('gulp-sourcemaps'),   // Source maps for SASS
-    cssmin      = require('gulp-clean-css'),    // CSS minimization
-    imagemin    = require('gulp-imagemin'),     // Image minimization
-    pngquant    = require('imagemin-pngquant'), // Lossy compression of PNG images
-    browserSync = require('browser-sync').create();      // Browser synchronization
+// del         - Files remove
+// gulpif      - Add some logic to pipes
+// twig        - twig parser
+// htmlmin     - html minimization
+// svgstore    - svg concatenation into symbol tags
+// svgmin      - svg minimization
+// rename      - File rename (for svg's id's)
+// rigger      - Files including (//= path/name)
+// prefixer    - Automatic adding prefix for CSS rules
+// uglify      - js minimization
+// scss        - SASS parser
+// sourcemaps  - Source maps for SASS
+// cssmin      - CSS minimization
+// imagemin    - Image minimization
+// pngquant    - Lossy compression of PNG images
+// browserSync - Browser synchronization
+//?concat      - File concatenation ["gulp-concat": "^2.6.0"]
+//?es          - ["event-stream": "^3.3.2"]
 
 var configFile  = 'config.yml',
+    gulp        = require('gulp'),
+    YAML        = require('yamljs'),
     config      = YAML.load(configFile),
-    dest = {}, src = {}, watch = {};
+    $ = {}, dest = {}, src = {}, watch = {};
+
+Object.keys(require('./package.json')['devDependencies'])
+    .forEach(function (pkg) {
+        $[pkg.replace('gulp-', '').replace(/-[a-z]/g, function(str, offs, s) {
+            return s[++offs].toUpperCase();
+        })] = require(pkg);
+    });
+var reload = $.browserSync.reload;
 
 function configUpdate() {
     config      = YAML.load(configFile);
@@ -56,32 +63,32 @@ gulp.task('config:update', function(done) {
 // SVG BUILD TASK
 gulp.task('svg:build', function() {
     return gulp.src(src.inlineSvg)
-        .pipe(gulpif(config.min.svg, svgmin()))
-        .pipe(rename(function (path) {          // Rename files for beauty id's
+        .pipe($.if(config.min.svg, $.svgmin()))
+        .pipe($.rename(function (path) { // Rename files for beauty id's
             var name = path.dirname.split(path.sep);
             name[0] == '.' ? name.shift() : true;
             name.push(path.basename);
             path.basename = name.join('-');
         }))
-        .pipe(svgstore({ inlineSvg: true }))    // svg concatenation into symbols
+        .pipe($.svgstore({ inlineSvg: true })) // svg concatenation into symbols
         .pipe(gulp.dest(dest.inlineSvg));
 });
 
 // HTML BUILD TASK
 gulp.task('html:build', gulp.series('svg:build', function() {
     return gulp.src(src.markup)
-        .pipe(twig({data: config}))
-        .pipe(gulpif(config.min.html, htmlmin({collapseWhitespace: true})))
+        .pipe($.twig({data: config}))
+        .pipe($.if(config.min.html, $.htmlmin({collapseWhitespace: true})))
         .pipe(gulp.dest(dest.markup));
-}, function() { return del([dest.inlineSvg]) }));
+}, function() { return $.del([dest.inlineSvg]) }));
 
 // IMAGE BUILD TASK
 gulp.task('img:build', function() {
     return gulp.src(src.image)
-        .pipe(gulpif(config.min.image, imagemin({
+        .pipe($.if(config.min.image, $.imagemin({
             progressive: true,
             svgoPlugins: [{ removeViewBox: false }],
-            use: [pngquant()],
+            use: [$.imageminPngquant()],
             interlaced: true
         })))
         .pipe(gulp.dest(dest.image));
@@ -91,38 +98,38 @@ gulp.task('img:build', function() {
 //     gulp.src(watch.style)
 //         .pipe(gulp.dest(dest.build + 'source/'))
 // });
-//
+
 // CSS BUILD TASK
 gulp.task('css:build', function() {
     return gulp.src(src.style)
-        .pipe(sourcemaps.init())                // Maps initialization
-        .pipe(scss())                           // scss compile
-        .pipe(prefixer(config.autoprefixer))
-        .pipe(gulpif(config.min.css, cssmin()))
-        .pipe(gulpif(config.min.css, rename({ suffix: '.min' })))
-        .pipe(sourcemaps.write('.'))               // Write maps
-        .pipe(gulp.dest(dest.style));       // Save to build path
+        .pipe($.sourcemaps.init())
+        .pipe($.scss())
+        .pipe($.autoprefixer(config.autoprefixer))
+        .pipe($.if(config.min.css, $.cleanCss()))
+        .pipe($.if(config.min.css, $.rename({ suffix: '.min' })))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest(dest.style));
 });
 
 // CSS CLEAN TASK
 gulp.task('css:clean', function() {
-    return del(['.gulp-scss-cache', '.sass-cache']);
+    return $.del(['.gulp-scss-cache', '.sass-cache']);
 });
 
 // JS BUILD TASK
 gulp.task('js:build', function() {
     return gulp.src(src.script)
-        .pipe(rigger())                         // Add dependent files
-        .pipe(sourcemaps.init())                // Maps initialization
-        .pipe(gulpif(config.min.js, uglify()))
-        .pipe(gulpif(config.min.js, rename({ suffix: '.min' })))
-        .pipe(sourcemaps.write())               // Write maps
-        .pipe(gulp.dest(dest.script));          // Save to build path
+        .pipe($.rigger()) // Add dependent files
+        .pipe($.sourcemaps.init())
+        .pipe($.if(config.min.js, $.uglify()))
+        .pipe($.if(config.min.js, $.rename({ suffix: '.min' })))
+        .pipe($.sourcemaps.write())
+        .pipe(gulp.dest(dest.script));
 });
 
 // CLEAN TASK
 gulp.task('clean', function() {
-    return del(dest.build);
+    return $.del(dest.build);
 });
 
 // BUILD TASK
@@ -130,16 +137,16 @@ gulp.task('build', gulp.series(gulp.parallel('css:build', 'js:build', 'img:build
 
 // SERVER AND BROWSER TASK
 gulp.task('sync', function () {
-    browserSync.init(config.browserSync);
+    $.browserSync.init(config.browserSync);
 });
 
 // WATCH TASK
 gulp.task('watch', function(done) {
-    gulp.watch(configFile, gulp.series('config:update', 'build', browserSync.reload));
-    gulp.watch([watch.markup, watch.inlineSvg], gulp.series('html:build', browserSync.reload));
-    gulp.watch(watch.style, gulp.series('css:build', browserSync.reload));
-    gulp.watch(watch.script, gulp.series('js:build', browserSync.reload));
-    gulp.watch(watch.image, gulp.series('img:build', browserSync.reload));
+    gulp.watch(configFile, gulp.series('config:update', 'build', reload));
+    gulp.watch([watch.markup, watch.inlineSvg], gulp.series('html:build', reload));
+    gulp.watch(watch.style, gulp.series('css:build', reload));
+    gulp.watch(watch.script, gulp.series('js:build', reload));
+    gulp.watch(watch.image, gulp.series('img:build', reload));
     done();
 });
 
